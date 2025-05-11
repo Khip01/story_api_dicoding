72c9d29ff4c946a64e547e573afc46567642a9ea
Khip01[desk] <aakhifathallah24@gmail.com>
2025-05-11 23:05:30 +0700

    chore: tidy up (linter changes)

--- lib/model/api_response.dart
+++ lib/model/api_response.dart
@@ -3,9 +3,22 @@ class ApiResponse<T> {
   final String message;
   final T? data;
 
-  ApiResponse({
-    required this.error,
-    required this.message,
-    required this.data,
-  });
+  ApiResponse({required this.error, required this.message, required this.data});
+
+  factory ApiResponse.fromJson(Map<String, dynamic> json, String? dataKeyName, Function(List<dynamic>) dataList) {
+
+    if (dataKeyName != null) {
+      return ApiResponse(
+        error: json["error"],
+        message: json["message"],
+        data: dataList(json[dataKeyName]),
+      );
+    }
+
+    return ApiResponse(
+      error: json["error"],
+      message: json["message"],
+      data: null,
+    );
+  }
 }
